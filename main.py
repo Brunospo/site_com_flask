fc0dded9c5bae6c4bac5682853fec18f3a9601cc
Bruno Bispo <brunobispoo25@gmail.com>
2025-06-13 14:11:24 -0300

    Projrto Flask: inicio do projeto

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+lista_usuarios = ['lira', 'Alon', 'Alessandra', 'Bia']
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/contato")
+def contatos():
+    return render_template('contato.html')
+
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios = lista_usuarios)
+
+if __name__ == '__main__':
+    app.run(debug=True)
