1eb45d84b76f3252cd8018f0959d21231ac789a9
Bruno Bispo <brunobispoo25@gmail.com>
2025-06-19 14:02:21 -0300

    Projeto Flask: Criptografando as senhas

--- comunidadeImpressionadora/routes.py
+++ comunidadeImpressionadora/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, request, flash, redirect
-from comunidadeImpressionadora import app
+from comunidadeImpressionadora import app, database, bcrypt
 from comunidadeImpressionadora.forms import FormCriarConta, FormLogin
 from comunidadeImpressionadora.models import Usuario
 
@@ -29,6 +29,10 @@ def login():
         flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
         return redirect(url_for('home'))
     if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
+        senha_crypt = bcrypt.generate_password_hash(form_criar_conta.senha.data)
+        usuario = Usuario(username = form_criar_conta.username.data, email = form_criar_conta.email.data, senha = senha_crypt)
+        database.session.add(usuario)
+        database.session.commit()
         flash(f'Conta criada com sucesso para o e-mail: {form_criar_conta.email.data}', 'alert-success')
         return redirect(url_for('home'))
 
