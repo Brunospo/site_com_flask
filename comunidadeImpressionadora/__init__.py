5ecd1bd82b28142086f135264f8a7fc7e8121084
Bruno Bispo <brunobispoo25@gmail.com>
2025-06-20 09:20:16 -0300

    Projeto Flask: Adicionando login ao site

--- comunidadeImpressionadora/__init__.py
+++ comunidadeImpressionadora/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -11,5 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 database = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
 
 from comunidadeImpressionadora import routes
--- comunidadeImpressionadora/routes.py
+++ comunidadeImpressionadora/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, request, flash, redirect
 from comunidadeImpressionadora import app, database, bcrypt
 from comunidadeImpressionadora.forms import FormCriarConta, FormLogin
 from comunidadeImpressionadora.models import Usuario
+from flask_login import login_user, logout_user
 
 lista_usuarios = ['lira', 'Alon', 'Alessandra', 'Bia']
 
@@ -26,8 +27,13 @@ def login():
     form_criar_conta = FormCriarConta()
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-        return redirect(url_for('home'))
+        usuario = Usuario.query.filter_by(email = form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember= form_login.lembrar_dados.data)
+            flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+            return redirect(url_for('home'))
+        else :
+            flash(f'Falha no Login, E-mail ou senha incorretos', 'alert-danger')
     if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
         senha_crypt = bcrypt.generate_password_hash(form_criar_conta.senha.data)
         usuario = Usuario(username = form_criar_conta.username.data, email = form_criar_conta.email.data, senha = senha_crypt)
